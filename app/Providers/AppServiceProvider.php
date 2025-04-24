f975e129b4be0d4e092a03b23f7487a61b4a2f92
vyPal <kubik.palacky@gmail.com>
2025-04-24 12:29:54 +0200

    Fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production' || config('app.env') === 'staging') {
+            URL::forceScheme('https');
+        }
     }
 }
