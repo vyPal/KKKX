7ecda287ea24403e6e1749c02aa7f976b5b6024f
vyPal <kubik.palacky@gmail.com>
2025-04-24 12:34:48 +0200

    Fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\ContentModerationService;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
